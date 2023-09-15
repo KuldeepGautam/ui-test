c3ae73f44b198f0ac09d21ed436c058c4b65d45d
Kuldeep Gautam <kuldeep.gautam@sisrtd.com>
2023-09-15 14:49:40 +0530

    Added pages

---
+++ src/components/navbar.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+          <div className="container">
+            <a className="navbar-brand">UPLOAD Logo</a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#mynavbar"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse " id="mynavbar">
+              <ul className="navbar-nav ">
+                <li className="nav-item">
+                  <a className="nav-link">Pages</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link">Feature</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link">Servies</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link">Blog</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link">Contact</a>
+                </li>
+              </ul>
+              {/* <form className="d-flex">
+                <input
+                  className="form-control me-2"
+                  type="text"
+                  placeholder="Search"
+                />
+                <button className="btn btn-primary" type="button">
+                  Search
+                </button>
+              </form> */}
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/components/pages/features.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Feature = () => {
+  return (
+    <>
+      <h2>Feature page.</h2>
+    </>
+  );
+};
+
+export default Feature;
